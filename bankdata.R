2a6ca840408b37b225be0e02e3a78b0831e6e9f2
pausar1 <sarkausk.p@gmail.com>
2019-03-30 11:17:15 +0200

    Delete bankdata.R

--- bankdata.R
+++
@@ -1,6 +0,0 @@
-setwd("C:/Users/Paulius/Documents/R/Bank marketing")
-
-bank <- read.csv(file = "bankfull.csv", header=TRUE, sep = ";")
-bank1 <- read.csv(file = "bank.csv", header=TRUE, sep = ";")
-
-hist(bank$age)
